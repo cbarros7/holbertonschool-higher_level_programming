243a08049505a4074ee4473b9f1de2dcce611e3c
cbarros7 <cj.barros07@gmail.com>
2020-04-27 16:40:37 -0500

    Fix error task#10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,18 +2,27 @@
 
 /**
  * check_cycle - list
- * @list:  type list
+ * @head:  type list
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-
-int check_cycle(listint_t *list)
+int check_cycle(listint_t *head)
 {
-	if (list == NULL)
-		return (0);
-	listint_t *node = list->next;
+	int *node1, *node2;
 
-	while (node != NULL && node != list)
-		node = node->next;
-	return (node == list);
-	return (1);
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		node1 = (int *)&head;
+		node2 = (int *)&head->next;
+		if (head->next == NULL)
+			return (0);
+
+		if (*node1 - *node2 <= 0)
+			return (1);
+
+		head = head->next;
+	}
+	return (0);
 }
