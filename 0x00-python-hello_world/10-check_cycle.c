7fa021df76f145dbf4269304cb2f1406848a0dff
cbarros7 <cj.barros07@gmail.com>
2020-04-27 15:57:09 -0500

    taksk #10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+
+/**
+ * check_cycle - list
+ * @list:  type list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
 int check_cycle(listint_t *list)
 {
 	if (list == NULL)
