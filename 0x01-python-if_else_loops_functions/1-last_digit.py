9c9cc78a41508e88f063700892804544522a46d8
cbarros7 <cj.barros07@gmail.com>
2020-04-28 10:25:01 -0500

    Fix error pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number % 10)
+ld = abs(number % 10)
 
 if number < 0:
-    last_digit *= -1
+    ld *= -1
 
-if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
-elif last_digit < 6 and last_digit !=0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+if ld > 5:
+    print("Last digit of %d is %d and is greater than 5" % (number, ld))
+elif ld < 6 and ld != 0:
+    print("Last digit of %d is %d and is less than 6 and not 0" % (number, ld))
 else:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+    print("Last digit of %d is %d and is 0" % (number, ld))
