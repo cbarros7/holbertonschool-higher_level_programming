d94b25820e8a75eeba43f9c06f7bd6545a6d42cf
cbarros7 <cj.barros07@gmail.com>
2020-04-28 16:20:03 -0500

    Add task #12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    if i % 5 == 0 and i % 3 == 0:
+        i = "FizzBuzz"
+        print("{:s} ".format(i), end="")
+    elif i % 5 == 0:
+        i = "Buzz"
+        print("{:s} ".format(i), end="")
+    elif i % 3 == 0:
+        i = "Fizz"
+        print("{:s} ".format(i), end="")
+    else:
+        print("{:d} ".format(i), end="")
