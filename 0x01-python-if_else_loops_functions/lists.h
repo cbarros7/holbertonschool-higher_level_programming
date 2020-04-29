38d2b540bb8b679427dd7246b1455b3d6e4c3203
cbarros7 <cj.barros07@gmail.com>
2020-04-28 22:16:09 -0500

    Add functions task #13

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,7 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-#include <stddef.h>
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
