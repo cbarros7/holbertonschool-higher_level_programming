5e8dea33dd187a48ce6160baa94209ab554da38d
cbarros7 <cj.barros07@gmail.com>
2020-04-30 14:38:43 -0500

    Fix errors task #1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
-
-
-def main():
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
-if __name__ == "__main__":
-    main()
