f0c8ee114e10a0322fd0f804b0e3fe01ff6dd257
cbarros7 <cj.barros07@gmail.com>
2020-04-30 15:20:12 -0500

    Task #2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv)
     if num == 1:
-        print("{:d} argument.".format(num - 1))
+        print("{:d} arguments.".format(num - 1))
     elif num == 2:
         print("{:d} argument:".format(num - 1))
     else:
