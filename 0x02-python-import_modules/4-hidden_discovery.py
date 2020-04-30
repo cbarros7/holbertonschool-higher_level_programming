224fa418c5e716a67c2a23c7ae2059417e058730
cbarros7 <cj.barros07@gmail.com>
2020-04-30 16:26:37 -0500

    Task #4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in range(names):
-        if names[:2] != "__":
-            print(name)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
