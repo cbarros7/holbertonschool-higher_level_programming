c6095ee8dd225edc577ec4ab9ebeac30b45c46b2
cbarros7 <cj.barros07@gmail.com>
2020-05-05 17:41:51 -0500

    Add task#9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,6 +8,6 @@ def max_integer(my_list=[]):
     for i in range(count):
         for j in range(i):
             if my_list[i] < my_list[j]:
-                continue
+                pass
             else:
                 return my_list[i]
