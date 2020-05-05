ab5c61d1085cb7d97fda042900dece826b9eaa97
cbarros7 <cj.barros07@gmail.com>
2020-05-05 13:58:13 -0500

    Fix error task# 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,11 @@
 
 
 def element_at(my_list, idx):
+    if my_list == []:
+        return None
     if idx < 0:
         return None
-    elif idx >= len(my_list):
+    if idx >= len(my_list):
         return None
     else:
        return my_list[idx]
