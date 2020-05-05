e1f7b74f543d6181ea70da99477e4c6632b67733
cbarros7 <cj.barros07@gmail.com>
2020-05-05 17:29:58 -0500

    Add task#8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,8 @@
 def multiple_returns(sentence):
     count = len(sentence)
     if count == 0:
-        return None
+        first = None
+        return count, first
     else:
         first = sentence[0]
         return count, first
