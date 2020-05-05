52abe6f6166e418efcd6827bb8b46822c25a5b64
cbarros7 <cj.barros07@gmail.com>
2020-05-05 18:40:51 -0500

    Add task#11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+
+    if idx > len(my_list) or idx < 0 or len(my_list) == 0:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
