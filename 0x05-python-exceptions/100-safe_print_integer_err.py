ff174fddab0e30b97fdd1c2457a24b9b19ab1ae3
cbarros7 <cj.barros07@gmail.com>
2020-05-18 14:58:36 -0500

    Fix errors task advanced

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=stderr)
         return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,13 +3,8 @@ from sys import stderr
 
 
 def safe_function(fct, *args):
-    result = 0
     try:
-        result = fct(args[0], args[1])
-        return result
-    except ZeroDivisionError as e:
-        print("Exception: {}".format(e), file=stderr)
-        return None
-    except IndexError as e:
+        return fct(*args)
+    except (ZeroDivisionError, IndexError) as e:
         print("Exception: {}".format(e), file=stderr)
         return None
