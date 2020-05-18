e77564a6c60ea370de3fc3874ac425c836434b10
cbarros7 <cj.barros07@gmail.com>
2020-05-18 12:34:37 -0500

    Fix errors task 1 and 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print(int("{:d}".format(value)))
-            return True
-    except:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
         return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         length += 1
     for j in range(x):
         try:
-            print("{}".format(int(my_list[j])), end="")
+            print("{:d}".format(my_list[j]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
