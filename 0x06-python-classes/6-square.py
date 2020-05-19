366f9bdad1143a729f49d45f00b3dff77a154eb0
cbarros7 <cj.barros07@gmail.com>
2020-05-19 17:03:58 -0500

    Fix error task #6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,14 +46,11 @@ class Square(object):
 
     def my_print(self):
         """Print vector with # and spaces"""
-        if self.__size > 0:
-            for k in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for space in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
-            print()
+        if self.__size == 0:
+            print("")
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
