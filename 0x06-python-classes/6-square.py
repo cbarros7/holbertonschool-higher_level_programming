7c6ccd6c0b03b7f2e6c0c86558dd5c8f19a822b7
cbarros7 <cj.barros07@gmail.com>
2020-05-19 16:31:36 -0500

    Fix error task #6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,6 +32,7 @@ class Square(object):
 
     @position.setter
     def position(self, value):
+        """Property for position"""
         error = "position must be a tuple of 2 positive integers"
         if type(value) is not tuple:
             raise TypeError(error)
@@ -48,14 +49,13 @@ class Square(object):
 
     def my_print(self):
         """Print vector with # and spaces"""
-        if self.__size > 0:
-            for k in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for space in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
-        # else:
-        #     print()
+        if self.__size == 0:
+            print("")
+        for k in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for space in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
