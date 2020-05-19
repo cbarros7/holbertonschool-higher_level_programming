d5718a2fe8daa60f8a519b54a4293da2edf8c8be
cbarros7 <cj.barros07@gmail.com>
2020-05-19 14:05:00 -0500

    Task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+# Carlos Barros <1543@holbertonschool.com>
+"""Private instance attribute: """
+
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, __size):
+        """initialize size"""
+        self.__size = 3
