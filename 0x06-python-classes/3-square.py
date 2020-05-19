30136ccd72e0a58dff1fc62bded0fb6c0541d573
cbarros7 <cj.barros07@gmail.com>
2020-05-19 14:05:18 -0500

    Task #3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 3-square.py
+# Carlos Barros <1543@holbertonschool.com>
+"""Area square: """
+
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, size=0):
+        """initialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
