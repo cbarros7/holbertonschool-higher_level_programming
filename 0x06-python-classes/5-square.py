abd58f479090edd3a9b5c01c0e86abece96ffbae
cbarros7 <cj.barros07@gmail.com>
2020-05-19 15:30:19 -0500

    Task #5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# 5-square.py
+# Carlos Barros <1543@holbertonschool.com>
+"""Printing a square: """
+
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, size=0):
+        """initialize size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """class variable size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Define area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print #"""
+        if self.__size == 0:
+            print("\n")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
