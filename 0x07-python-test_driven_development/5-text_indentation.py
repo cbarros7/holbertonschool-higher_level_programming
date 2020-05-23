c27d2829bf75a4596bcec2b5ce67dd5bec6c665a
cbarros7 <cj.barros07@gmail.com>
2020-05-22 23:18:41 -0500

    Task #4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# 5-text_indentation.py
+# Carlos Barros <1543@holbertonschool.com>
+""" File name : 5-text_indentation.py
+    Text indentation :  function that prints a text with 2 
+    new lines after each of these characters: ., ? and :
+    Prototype: def text_indentation(text)
+"""
+
+def text_indentation(text):
+    """function that prints a square with the character #
+    size: numbers the character # to print 
+    Raises:
+        TypeError: size must be an integer
+        TypeError: size must be >= 0
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    phrase = ""
+    i = 0
+    while i < len(text):
+        if text[i] != "." and text[i] != "?" and text[i] != ":":
+            phrase += text[i]
+        
+        else: 
+            phrase += text[i]
+            print(phrase)
+            print()
+            phrase = ""
+            while i < (len(text) - 1) and text[i+1] == " ":
+                i += 1
+        i += 1
+    print(phrase,end="")
