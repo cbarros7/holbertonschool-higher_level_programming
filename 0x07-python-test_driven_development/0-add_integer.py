a40b6200b9983074a7b14310f8b9d0076dcd3a4d
cbarros7 <cj.barros07@gmail.com>
2020-05-23 10:47:58 -0500

    Add conditionals

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,8 +14,15 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
-    if (not isinstance(a, int) and (not isinstance(a, float))):
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    if (a is None or not isinstance(a, int) and (not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     return int(a) + int(b)
