2d2e2ca78853baa2d330a170dd16f47632cce9cd
cbarros7 <cj.barros07@gmail.com>
2020-05-23 11:01:48 -0500

    Fix error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,7 +18,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
     return int(a) + int(b)
