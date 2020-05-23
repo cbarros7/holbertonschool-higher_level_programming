9aa6e27fd9188a9bf7b93d86104e44b4d460a57e
cbarros7 <cj.barros07@gmail.com>
2020-05-22 23:32:29 -0500

    Fix errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,16 +6,15 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Define unittests for max_integer([..])."""
     def test_ordered_list(self):
         """Define ordered list"""
         ordered_list = [1, 2, 3, 4]
         self.assertEqual(max_integer(ordered_list), 4)
-        
         with self.assertRaises(TypeError):
             max_integer([1, '2', 3])
-
         with self.assertRaises(TypeError):
             max_integer([1, [2], 3])
 
@@ -23,19 +22,18 @@ class TestMaxInteger(unittest.TestCase):
         """Define unordered list"""
         unordered_list = [5, 6, 9, 20]
         self.assertEqual(max_integer(unordered_list), 20)
-        
+
     def test_empty_list(self):
         """Define empty list"""
         empty_list = []
         self.assertEqual(max_integer(empty_list), None)
-    
+
     def test_p_float_list(self):
         """Define postive float list"""
         p_float_list = [8.74, 2.54, 10.54, 4.77]
         self.assertEqual(max_integer(p_float_list), 10.54)
         with self.assertRaises(TypeError):
             max_integer([1, '2', 3.65])
-
         with self.assertRaises(TypeError):
             max_integer([1, [2.53], 3.34])
 
@@ -48,7 +46,7 @@ class TestMaxInteger(unittest.TestCase):
         """Define negative float list"""
         n_float_list = [8.74, -2.54, -10.54, 4.77]
         self.assertEqual(max_integer(n_float_list), 8.74)
-    
+
     def test_n_2_float_list(self):
         """Define negative float list"""
         n_2_float_list = [-8.74, -2.54, -10.545, -10.5458]
@@ -58,7 +56,7 @@ class TestMaxInteger(unittest.TestCase):
         """Define combine integer and float list"""
         int_float_list = [0.47, 4, 5.6, 6.6, 3, 10.43]
         self.assertEqual(max_integer(int_float_list), 10.43)
-    
+
     def test_word_lw_list(self):
         """Define lowercase and uppercase word list"""
         word_lw_list = ["Holberton", "School", "SC"]
@@ -73,12 +71,12 @@ class TestMaxInteger(unittest.TestCase):
         """Define lowercase word with numbers list"""
         word_lw_num_list = ["Holberton", "School", "School444"]
         self.assertEqual(max_integer(word_lw_num_list), "School444")
-    
+
     def test_word_up_num_list(self):
         """Define uppercase word with numbers list"""
         word_up_num_list = ["Holberton", "School", "SCHOOL444"]
         self.assertEqual(max_integer(word_up_num_list), "School")
-    
+
     def test_beg_space_list(self):
         """Define begining spaces word list"""
         beg_space_list = ["Holberton", "        School", " School"]
@@ -88,7 +86,7 @@ class TestMaxInteger(unittest.TestCase):
         """Define word with character special list"""
         chr_special_list = ["Holberton", "        School", "!School    "]
         self.assertEqual(max_integer(chr_special_list), "Holberton")
-    
+
     def test_chr_special_2_list(self):
         """Define other word with character special list"""
         chr_special_2_list = ["Holberton", "School!3? ", "School !?3    "]
@@ -96,4 +94,3 @@ class TestMaxInteger(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
