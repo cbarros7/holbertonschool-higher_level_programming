a3e90dd66e96496dc2a604fae03430c9da2da841
cbarros7 <cj.barros07@gmail.com>
2020-05-22 23:35:07 -0500

    Task #2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,12 +10,12 @@
 
 def say_my_name(first_name, last_name=""):
     """Return string with full name
-    first_name and last_name must be strings 
+    first_name and last_name must be strings
     Raises:
         TypeError: first_name must be a string
         TypeError: first_name must be a string
     """
-    if not isinstance(first_name,str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
