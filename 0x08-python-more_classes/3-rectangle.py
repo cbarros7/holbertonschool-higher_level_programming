e8617bb771d92e54fc02433833ac2813b2398f64
cbarros7 <cj.barros07@gmail.com>
2020-05-25 20:24:01 -0500

    Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -9,29 +9,20 @@
 
 class Rectangle(object):
     """Rectangle: Define new class"""
-    def __init__(self, height=0, width=0):
-        """Initialize new class rectangle
-
-        Args:
-            width (int): width for the new rectangle
-            height (int): height for the new rectangle
-        """
+    def __init__(self, width=0, height=0):
+        """ initializes the width and height """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Property for attribute width."""
+        """ gets the width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set values to width
-
-        Args:
-            value (int): new value for width
-        """
-        if not isinstance(value, int):
+        """ sets the width """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -39,38 +30,36 @@ class Rectangle(object):
 
     @property
     def height(self):
-        """Property for attribute height."""
+        """ gets the height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set values to height
-
-        Args:
-            value (int): new value for height
-        """
-        if not isinstance(value, int):
+        """ sets the height """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Define area"""
-        return self.__width * self.__height
+        """ method to find the area of a rectangle """
+        return self.__height * self.__width
 
     def perimeter(self):
-        """Define perimeter"""
-        if self.__width == 0 or self.__height == 0:
+        """ method to find the perimeter of a rectangle """
+        if self.__height == 0 or self.__width == 0:
             return 0
-        return (2 * self.__width) + (2 * self.__height)
+        return (self.__height * 2) + (self.__width * 2)
+
     def __str__(self):
         """ string representation of square """
         string = ""
         if self.__height == 0 or self.__width == 0:
-            return ""
-        wide = "#" * self.width
-        for i in range(self.height - 1):
-            string += wide + "\n"
-        string += wide
-        return str(string)
+            return string
+        for y in range(self.__height):
+            for x in range(self.__width):
+                string += "#"
+            string += "\n"
+        string = string[:-1]
+        return string
