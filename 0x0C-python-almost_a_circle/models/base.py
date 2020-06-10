f970b47cc304d3ba8b70de3761312531cb81209c
cbarros7 <cj.barros07@gmail.com>
2020-06-10 09:36:49 -0500

    Fix error models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,7 +51,7 @@ class Base(object):
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None or json_string == "[]":
-            return ('[]')
+            return ("[]")
         else:
             return json.loads(json_string)
 
