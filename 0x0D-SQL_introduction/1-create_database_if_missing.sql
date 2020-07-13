82d645fdb9562952ddc55bc4e10024f3e19a2f46
cbarros7 <cj.barros07@gmail.com>
2020-07-13 19:10:58 +0000

    Add task 1 and 2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+#!/bin/bash/env mysql
+-- Create databases
+CREATE DATABASES IF NOT EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+#!/bin/bash/env mysql
+-- Delete databases
+DROP DATABASE [IF EXISTS] hbtn_0c_0;
+
