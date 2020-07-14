ed13b5f814614170c8abf00a331a526cf7627b94
cbarros7 <cj.barros07@gmail.com>
2020-07-14 23:47:32 +0000

    Fix error task 13 and 14

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Genre ID for all show
-SELECT tv_genres.name genre, tv_show_genres.genre_id number_of_shows
+SELECT tv_genres.name genre, COUNT(tv_show_genres.genre_id) number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id 
 GROUP BY tv_genres.name, tv_show_genres.genre_id
-ORDER BY tv_show_genres.genre_id DESC;
+ORDER BY number_of_shows DESC;
 
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- My genres
 SELECT tv_genres.name name
 FROM tv_genres
-INNER JOIN tv_show_genres 
+JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id 
 WHERE tv_show_genres.show_id = 8
 GROUP BY tv_genres.name, tv_show_genres.genre_id
-ORDER BY tv_show_genres.genre_id ASC;
+ORDER BY tv_genres.name ASC;
 
