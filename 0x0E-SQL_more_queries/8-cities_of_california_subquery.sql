7c8d75c947129534abf263d061fc836b13776adb
cbarros7 <cj.barros07@gmail.com>
2020-07-14 21:36:55 +0000

    Fix error task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 -- List all cities of California
 SELECT id, name 
-FROM hbtn_0d_usa.cities 
-WHERE state_id = 1
-ORDER BY cities.id ASC; 
+FROM cities
+WHERE state_id IN (
+SELECT id
+FROM states
+WHERE name LIKE '%California%')
+ORDER BY id ASC; 
 
