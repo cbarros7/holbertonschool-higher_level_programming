4ab29a9cfdee7bdebd97ea208204c633df459477
cbarros7 <cj.barros07@gmail.com>
2020-07-14 23:55:09 +0000

    Fix error task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,10 @@
 SELECT tv_genres.name name
 FROM tv_genres
 JOIN tv_show_genres 
-ON tv_genres.id = tv_show_genres.genre_id 
-WHERE tv_show_genres.show_id = 8
-GROUP BY tv_genres.name, tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_shows.title LIKE '%Dexter%'
+-- GROUP BY tv_genres.name, tv_show_genres.genre_id
 ORDER BY tv_genres.name ASC;
 
