b77463a27ad1f7738a9faf11d12e153d713350c2
cbarros7 <cj.barros07@gmail.com>
2020-07-14 22:03:18 +0000

    Add task 9-10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Genre ID by show
+SELECT s.title, g.genre_id
+FROM tv_shows s
+JOIN tv_show_genres g 
+ON s.id = g.genre_id 
+ORDER BY s.title, g.genre_id;
+ 
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Cities by states
+SELECT cities.id, cities.name, states.name
+FROM cities 
+JOIN states
+ON cities.state_id = states.id 
+ORDER BY cities.id ASC;
+
