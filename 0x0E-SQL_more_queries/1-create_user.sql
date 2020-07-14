963ca58a317df3b588a3a7d44d7c04f2b5402df2
cbarros7 <cj.barros07@gmail.com>
2020-07-14 18:08:19 +0000

    Fix error task 0 and 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create the MySQL server user user_0d_1
-CREATE USER IF NO EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.*
+TO 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
