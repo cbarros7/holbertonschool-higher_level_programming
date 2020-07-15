d114f4d36dd82ba61a722d284ef129a91276e616
cbarros7 <cj.barros07@gmail.com>
2020-07-15 00:12:19 +0000

    Add task 14-16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@ LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id 
 LEFT OUTER JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title, tv_genres.name ASC;
 
