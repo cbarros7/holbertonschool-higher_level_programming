a61a94779af78bf1910dfc1d87f88e988181b899
cbarros7 <cjbarros07@gmail.com>
2020-08-09 11:41:39 -0500

    Add task 6 - 8

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Initialize engine
+    Base.metadata.create_all(engine)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query
+    query = session.query(State).order_by(State.id).all()
+
+    # Print query
+    for record in query:
+        print("{}: {}".format(record.id, record.name))
+
+    # Close session
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Prints the first State object from the
+database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Initialize engine
+    Base.metadata.create_all(engine)
+
+    # Initialize session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query
+    query = session.query(State).first()
+
+    # Print query
+    print("{}: {}".format(query.id, query.name))
+
+    # Close session
+    session.close()
