e2276b7ee1833d5a16e4042d592dbd3206fa26f8
cbarros7 <cjbarros07@gmail.com>
2020-08-09 13:08:53 -0500

    Fix task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     session = Session()
 
     # Add new record
-    new_state = State(id=6, name="Louisiana")
+    new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
 
