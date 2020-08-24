f3284c9148094b7fb5040300d6026ad012d6707b
cbarros7 <cj.barros07@gmail.com>
2020-08-24 17:53:29 -0500

    Upload task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Search API """
+import requests
+import json
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        a_dict = {'q': ''}
+    elif len(sys.argv) > 1:
+        a_dict = {'q': sys.argv[1]}
+    req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
+    if 'json' not in req.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        if req.json():
+            print('[{}] {}'.format(req.json().get('id'),
+                                   req.json().get('name')))
+        else:
+            print('No result')
