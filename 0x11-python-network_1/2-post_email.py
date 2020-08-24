a56e436c46ded04c99122058f78d3b318193c858
cbarros7 <cj.barros07@gmail.com>
2020-08-24 12:52:44 -0500

    Update task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     email = sys.argv[2]
-    p_data = urllib.parse.urlencode({"a_key": email}).encode('ascii')
+    p_data = urllib.parse.urlencode({"a_key": email}).encode('utf-8')
     with urllib.request.urlopen(sys.argv[1], data=p_data) as url:
         s = url.read()
         print("Your email is: {}".format(email))
