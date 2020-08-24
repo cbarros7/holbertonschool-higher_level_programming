8cef6c00f08491ea6bdf8beeca1f6dbf1ac71834
cbarros7 <cj.barros07@gmail.com>
2020-08-24 17:59:34 -0500

    Update task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
         a_dict = {'q': ''}
     elif len(sys.argv) > 1:
         a_dict = {'q': sys.argv[1]}
-    req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
-    if 'json' not in req.headers.get('content-type'):
-        print("Not a valid JSON")
-    else:
+
+    try:
+        req = requests.post('http://0.0.0.0:5000/search_user', a_dict)
         if req.json():
             print('[{}] {}'.format(req.json().get('id'),
                                    req.json().get('name')))
         else:
             print('No result')
+            
+    except ValueError:
+        print("Not a valid JSON")
