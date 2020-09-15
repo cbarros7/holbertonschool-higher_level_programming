65387da7f990fef559cef303d425853a780fb47d
cbarros7 <cj.barros07@gmail.com>
2020-09-15 16:10:37 -0500

    Add task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ for (let i = 2; i < process.argv.length; i++) {
   lista.push(number);
 }
 if (lista.length < 2) {
-  console.log('0');
+  console.log(0);
 } else {
   console.log(lista.sort().slice(-2)[0]);
 }
