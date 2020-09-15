5d464f0681dbea82f9789894381c75572de5bff6
cbarros7 <cj.barros07@gmail.com>
2020-09-15 15:11:42 -0500

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const phrases = 'X';
+const num = process.argv[2];
+if (parseInt(num)) {
+  for (let step = 0; step < num; step++) {
+    console.log(phrases.repeat(num));
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
