2201d22c7dcc37d1a412411bcd105d243e27c6b8
cbarros7 <cj.barros07@gmail.com>
2020-09-15 13:30:13 -0500

    Add task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-let str;
-if (process.argv.length < 3) {
-  str = 'No argument';
-} else {
-  str = process.argv[2];
-}
-console.log(str);
+console.log(process.argv[2] || 'No argument');
