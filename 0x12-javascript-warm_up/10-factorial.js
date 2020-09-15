86f3ecf41ebf950bb9e280a29b5da5c48158a236
cbarros7 <cj.barros07@gmail.com>
2020-09-15 15:31:18 -0500

    Add task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,8 +3,9 @@ const factorial = function fac (n) {
   const number = n < 2 ? 1 : n * fac(n - 1);
   if (isNaN(number)) {
     return 1;
+  } else {
+    return number;
   }
-  return number;
 };
 
 console.log(factorial(process.argv[2]));
