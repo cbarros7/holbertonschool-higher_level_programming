bb5a3129d3397a3eafa788a5a9234342f8c451af
cbarros7 <cj.barros07@gmail.com>
2020-09-15 16:04:55 -0500

    Update task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let lista = [];
+const lista = [];
 for (let i = 2; i < process.argv.length; i++) {
   lista.push(process.argv[i]);
 }
-if (process.argv.length < 4) {
-  console.log(0);
+if (process.argv.length < 3) {
+  console.log('0');
 } else {
   console.log(Number(lista.sort().slice(-2)[0]));
 }
