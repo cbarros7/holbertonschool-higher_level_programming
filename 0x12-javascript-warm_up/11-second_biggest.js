7faa5c35f820a2236a52021ba3dbfc5681f44b95
cbarros7 <cj.barros07@gmail.com>
2020-09-15 16:15:54 -0500

    Add task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const lista = [];
 for (let i = 2; i < process.argv.length; i++) {
-  const number = parseInt(process.argv[i]);
-  lista.push(number);
+  lista.push(process.argv[i]);
 }
 if (lista.length < 2) {
   console.log(0);
