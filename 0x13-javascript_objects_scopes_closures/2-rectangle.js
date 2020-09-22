b2c086132c76a4ea80a517f4ea2db932bbe868b4
cbarros7 <cj.barros07@gmail.com>
2020-09-22 01:58:22 +0000

    Update 2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Define class Rectangle with conditional === 0 or not positive integer
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
       this.width = w;
       this.height = h;
