3012ab85504f4b529c86b67ebf41c550b080e829
cbarros7 <cj.barros07@gmail.com>
2020-09-21 21:01:30 -0500

    Add task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Define class Rectangle with conditional === 0 or not positive integer
+class Rectangle {
+  constructor(w, h) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    console.log(
+      ('X'.repeat(this.width) + '\n')
+        .repeat(this.height)
+        .split('')
+        .slice(0, -1)
+        .join('')
+    );
+  }
+}
+
+module.exports = Rectangle;
